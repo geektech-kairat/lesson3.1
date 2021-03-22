38731dc0dbc2926c5e83cf354e049bf5f1865cd8
kairat <goold1999@mail,,ru>
2021-03-22 13:50:40 +0600

    hi

--- app/src/main/java/com/example/lesson3/MainActivity.java
+++ app/src/main/java/com/example/lesson3/MainActivity.java
@@ -4,10 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Color;
 import android.graphics.PorterDuff;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,16 +14,34 @@ import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     TextView weather;
     TextView weatherDown;
+    Document doc;
 
-    ImageButton camera;
-    ImageButton call;
-    ImageButton chrome;
+    Thread myThread;
+    Runnable runnable;
 
+
+    public void getData() {
+        try {
+            doc = Jsoup.connect("https://world-weather.ru/pogoda/kyrgyzstan/bishkek/").get();
+            Elements elements = doc.getElementsByClass("weather-now-number");
+            Log.e("ololo", "getData: ");
+            Element element = elements.get(0);
+        } catch (Exception e) {
+            Log.e("ololo", "getData: ", e);
+        }
+    }
 
     private String[] regions = {"Бишкек", "Ош", "Ысык-кол", "Баткен", "Чуй", "Талас", "Джала-Абад"};
 
@@ -32,27 +49,19 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
+        setMyThread();
         weather = findViewById(R.id.weather);
         weatherDown = findViewById(R.id.weatherDown);
 
-        camera = findViewById(R.id.camera);
-        chrome = findViewById(R.id.chrome);
-        call = findViewById(R.id.call);
-
-        intent();
-
         region();
-
-
     }
 
     public void region() {
 
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, regions);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.item, regions);
 
         Spinner spinner = findViewById(R.id.spinner_city);
         spinner.getBackground().setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
@@ -94,20 +103,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     }
 
-    public void intent() {
-        call.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, Call.class);
-            startActivity(intent);
-
-        });
-        camera.setOnClickListener(v -> {
-            Intent intent2 = new Intent(MainActivity.this, Camera.class);
-            startActivity(intent2);
-        });
-        chrome.setOnClickListener(v -> {
-            Intent intent3 = new Intent(MainActivity.this, Chrome.class);
-            startActivity(intent3);
-
-        });
+    public void setMyThread(){
+        runnable = this::getData;
+        myThread = new Thread(runnable);
+        myThread.start();
     }
+
+
 }
